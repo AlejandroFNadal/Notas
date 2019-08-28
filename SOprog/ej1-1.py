914b80a2a78e4791531848067a0340c7917fe443
AnastaciusWright <32724827+AnastaciusWright@users.noreply.github.com>
2019-08-28 19:43:28 -0300

    Add files via upload

---
+++ SOprog/ej1-1.py
@@ -0,0 +1,14 @@
+
+#Ejercicio Practico 1.1 Sist Op
+print("Ingrese tamano de memoria");
+tamMemoria=int(input())
+print("Ingrese cantidad de particiones");
+cantParticiones=int(input())
+
+#Crear memoria vacia
+memoria=[]
+i = 0
+for i in range(0,tamMemoria):
+    memoria.append("_")
+print(memoria)
+    
